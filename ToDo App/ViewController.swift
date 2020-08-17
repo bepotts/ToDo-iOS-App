72dbd0c1b62733bff6d1e3a9b223452121021076
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-08-16 20:44:42 -0400

    Initial Commit

---
+++ ToDo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo App
+//
+//  Created by Brandon Potts on 8/16/20.
+//  Copyright © 2020 Brandon Potts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
